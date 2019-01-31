26dcf201737c57188e5329fa017001884eaf30fe
Arino <arinojix@emea.roche.com>
2019-01-31 15:01:56 +0100

    fisrt

---
+++ src/Addition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Addition {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter numbers to add:");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("El resultado es: " + (a + b));
+
+    }
+
+}
